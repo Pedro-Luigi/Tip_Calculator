4b71bd87fe8e5f1fe945559f9e5f0a6cbac4f967
Pedro Rezende <68929840+Pedro-Luigi@users.noreply.github.com>
2023-01-24 17:50:36 -0300

    Add tip rounding.

--- app/src/main/java/com/meu/tiptime/MainActivity.kt
+++ app/src/main/java/com/meu/tiptime/MainActivity.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -53,7 +54,10 @@ class MainActivity : AppCompatActivity() {
         val amount = amountInput.toDoubleOrNull() ?: 0.0
         var tipInput by remember { mutableStateOf("") }
         val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
-        val tip = calculateTip(amount, tipPercent)
+        var roundUp by remember { mutableStateOf(false) }
+        val tip = calculateTip(amount, tipPercent, roundUp)
+
+        val focusManager = LocalFocusManager.current
 
         Column(
             Modifier.padding(32.dp), Arrangement.spacedBy(8.dp)
@@ -70,12 +74,18 @@ class MainActivity : AppCompatActivity() {
                 label = R.string.bill_amount,
                 value = amountInput,
                 onValueChange = {amountInput = it},
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next))
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
+                keyboardActions = KeyboardActions(onNext = {focusManager.moveFocus(FocusDirection.Down)})
+            )
             editNumberField(
                 label = R.string.how_was_the_service,
                 value = tipInput,
                 onValueChange = {tipInput = it},
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done))
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {focusManager.clearFocus()})
+            )
+            //Adicionando Switch
+            roundTheTipRow(roundUp = roundUp, onRoundUpChange = {roundUp = it})
 
             //Adiciona espaço
             Spacer(Modifier.height(24.dp))
@@ -96,6 +106,7 @@ class MainActivity : AppCompatActivity() {
         value: String,
         onValueChange: (String) -> Unit,
         keyboardOptions: KeyboardOptions,
+        keyboardActions: KeyboardActions,
         modifier: Modifier = Modifier
     ) {
         TextField(
@@ -103,17 +114,43 @@ class MainActivity : AppCompatActivity() {
             onValueChange = onValueChange,
             label = { Text(text = stringResource(id = label), Modifier.fillMaxWidth())}   ,
             keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
             singleLine = true
         )
 
 
     }
 
+    @Composable
+    private fun roundTheTipRow(
+        roundUp:Boolean,
+        onRoundUpChange: (Boolean) -> Unit,
+        modifier: Modifier = Modifier
+    ){
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .size(48.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = stringResource(id = R.string.round_up_tip))
+            Switch(checked = roundUp, onCheckedChange = onRoundUpChange,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End),
+                colors = SwitchDefaults.colors(uncheckedThumbColor = Color.DarkGray)
+            )
+        }
+    }
+
+
     private fun calculateTip(
         amount: Double,
-        tipPercent: Double = 15.0
+        tipPercent: Double = 15.0,
+        roundUp: Boolean
     ): String {
-        val tip = tipPercent / 100 * amount
+        var tip = tipPercent / 100 * amount
+        if (roundUp) tip = kotlin.math.ceil(tip)
         return NumberFormat.getCurrencyInstance().format(tip)
     }
 }
